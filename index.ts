a1466ce0ed03214696e1eb7bf6ff68ebe2406e1a
Nickwasused <github.lix1s@simplelogin.co>
2022-12-15 14:41:16 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,78 @@
+async function get_nearest(lat: number, long: number): Promise<stationdata[]> {
+    const station_data = await fetch(`https://tankstellenfinder.aral.de/api/v1/locations/nearest_to?lat=${lat}&lng=${long}&autoload=true&travel_mode=driving&avoid_tolls=false&avoid_highways=false&show_stations_on_route=true&corridor_radius=5&format=json`);
+    if (station_data.status == 429) {
+        console.warn("We are getting rate limited!")
+        throw new Error("rate-limit");
+    } else {
+        return station_data.json()
+    }
+}
+
+type stationdata = {
+    id: string,
+    name: string,
+    lat: string,
+    lng: string,
+    adress: string,
+    city: string,
+    state: string,
+    postcode: string,
+    country_code: string,
+    telephone: string,
+    facilities: string[],
+    products: string[],
+    opening_hours: string[],
+    open_status: string,
+    site_brand: string,
+    watchlist_id?: string,
+    website?: string
+}
+
+
+async function get_all_stations(): Promise<stationdata[]> {
+    let station_data: stationdata[] = [];
+    // latitude and longitude are the bounds of germany
+    for (let i=47.2; i<55.2; i=i+0.5) {
+        for (let x=5.7; x<15.2; x=x+0.5) {
+            console.log(`${i}:${x}`)
+            let fetching = true;
+            let current_station_data: stationdata[] = [];
+            while (fetching) {
+                try {
+                    const tmp_station_data: stationdata[] = await get_nearest(i, x);
+                    fetching = false
+                    current_station_data = tmp_station_data
+                } catch (error) {
+                    console.error(error)
+                    await new Promise(res => setTimeout(() => res(""), 120000));
+                }
+            }
+            station_data = [...station_data, ...current_station_data]
+        }
+    }
+
+    return station_data
+}
+
+const original_station_data: stationdata[] = await get_all_stations();
+
+// filter for country code
+const filtered_station_data: stationdata[] = original_station_data.filter((station) => (station.country_code == "DE"));
+
+function deduplicate_stations(array: stationdata[]): stationdata[] {
+    const uniqueIds = new Set<string>();
+    return array.filter((element: stationdata) => {
+        if (!uniqueIds.has(element.id)) {
+            uniqueIds.add(element.id);
+            return element;
+        }
+    });
+}
+
+// remove duplicates
+const unique_station_data = deduplicate_stations(filtered_station_data);
+
+
+console.log(`got ${unique_station_data.length} stations`)
+
+Deno.writeTextFile("./stations.json", JSON.stringify(unique_station_data))
