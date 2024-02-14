f4acebd1c3dcee1af8afc4dc42da7e61e419a5a8
Nickwasused <github.lix1s@simplelogin.co>
2024-02-14 16:41:00 +0100

    format file and fix typo

--- database.py
+++ database.py
@@ -5,9 +5,11 @@ from json import load, dumps
 import argparse
 import sqlite3
 
+
 def load_json(file):
     f = open(file, encoding="utf8")
     return load(f)
+
 
 station_path = Path(__file__).parent.absolute().joinpath('./out/stations_min.json')
 output_path = Path(__file__).parent.absolute().joinpath('./out/aral.db')
@@ -25,14 +27,16 @@ cursor.execute(facilities_query)
 
 connection.commit()
 
+
 def export_facilities():
     station_data = load_json(station_path)
     unique_facilities = []
     for station in station_data:
-        for facilitie in station["facilities"]:
-            if facilitie not in unique_facilities:
-                unique_facilities.append(facilitie)
-                cursor.execute("INSERT INTO facilities (name) VALUES(?)", (facilitie,))
+        for facility in station["facilities"]:
+            if facility not in unique_facilities:
+                unique_facilities.append(facility)
+                cursor.execute("INSERT INTO facilities (name) VALUES(?)", (facility,))
+
 
 def export_fuel():
     station_data = load_json(station_path)
@@ -43,29 +47,34 @@ def export_fuel():
                 unique_fuel.append(fuel)
                 cursor.execute("INSERT INTO fuel (name) VALUES(?)", (fuel,))
 
+
 def export_stations():
     station_data = load_json(station_path)
     for station in station_data:
         try:
-            cursor.execute("INSERT INTO stations (id,name,lat,lng,address,city,state,postcode,country_code,telephone,site_brand,watchlist_id,website,fuel,facilities) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", (
-                station["id"],
-                station["name"],
-                station["lat"],
-                station["lng"],
-                station["address"],
-                station["city"],
-                station["state"],
-                station["postcode"],
-                station["country_code"],
-                station["telephone"],
-                station["site_brand"],
-                station["watchlist_id"],
-                station["website"],
-                ', '.join(station["products"]),
-                ', '.join(station["facilities"])
-            ))
+            cursor.execute(
+                "INSERT INTO stations (id,name,lat,lng,address,city,state,postcode,country_code,telephone,site_brand,"
+                "watchlist_id,website,fuel,facilities) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);",
+                (
+                    station["id"],
+                    station["name"],
+                    station["lat"],
+                    station["lng"],
+                    station["address"],
+                    station["city"],
+                    station["state"],
+                    station["postcode"],
+                    station["country_code"],
+                    station["telephone"],
+                    station["site_brand"],
+                    station["watchlist_id"],
+                    station["website"],
+                    ', '.join(station["products"]),
+                    ', '.join(station["facilities"])
+                ))
         except sqlite3.IntegrityError:
             print(f"error while inserting {station['id']}")
+
 
 if __name__ == "__main__":
     export_facilities()
