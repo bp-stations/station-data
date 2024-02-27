7de1e26ff4ff34f38460cf8f3403c3fa43e734cd
Nickwasused <github.lix1s@simplelogin.co>
2024-02-27 11:33:18 +0100

    Delete kml.py

--- kml.py
+++
@@ -1,6 +0,0 @@
-import simplekml
-
-    kml = simplekml.Kml()
-
-kml.save("test.kml")
-kml.newpoint(name=f'[DE-{point["postcode"]}] {point["name"]}; {point["address"]} [{point["city"]}]>[{point["telephone"]}]', coords=[(point["lng"], point["lat"])])
